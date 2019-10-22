62dc6aad007675777f0d66f9af4afd3520890e30
Alex Kashko <alexkrond@yahoo.com>
2019-10-22 20:34:12 +0300

    Добавлено обновление курсов валют по расписанию и заглушка 404

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const cron = require("node-cron");
 
-const api = require("./routes/api/api.js");
 const app = express();
+const currencies = ["USD", "EUR"];
+const exchangeRates = {};
+const updateExchangeRates = require("./scripts/updateExchangeRates.js")(exchangeRates, currencies);
+const api = require("./routes/api/api.js")(exchangeRates);
 
 
-cron.schedule("0 0 */1 * * *", () => {
-  console.log("TEST");
-}, {});
-
-
-// app.get("/", (req, res) => {
-//   res.send("Hi");
-// });
+cron.schedule("0 0 */1 * * *", updateExchangeRates, {});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -20,9 +16,15 @@ app.use(express.urlencoded({extended: false}));
 app.use("/api", api);
 app.use(express.static("public"));
 
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/public/404.html");
+});
 
 const PORT = process.env.PORT || 5000;
+(async () => {
+  await updateExchangeRates();
 
-app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+  });
+})();
