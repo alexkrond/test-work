9e04629aa8ead02d28c4d97e555941ad3942cd03
Alex Kashko <alexkrond@yahoo.com>
2019-10-22 20:34:42 +0300

    Добавлена обработка роута getprices

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const calculatePrices = require("../../scripts/calculatePrices.js");
 
-const router = express.Router();
+function initRouter(exchangeRates) {
+  const router = express.Router();
 
-router.post("/getprices", (req, res) => {
-  console.log(req.body);
-  res.status(200).end();
-});
+  router.post("/getprices", (req, res) => {
+    let prices;
 
-module.exports = router;
+    if (req.body.hasOwnProperty("products")) {
+      try {
+        prices = calculatePrices(req.body.products, exchangeRates);
+        res.status(200).send({...prices});
+      } catch (e) {
+        res.status(400).send({status: "false", msg: e.message});
+      }
+    } else {
+      res.status(400).send({status: "false", msg: "В запросе отсутствует поле products"});
+    }
+  });
+
+  return router
+}
+
+module.exports = initRouter;
